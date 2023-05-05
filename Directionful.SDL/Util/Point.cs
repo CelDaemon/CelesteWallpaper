bab59ec30f7c7d2b2f298f37b4ea33f1c9737625
DeVoid <devoid@voidgroup.net>
2023-05-05 15:04:02 +0200

    Add Basic SDL Rendering Support (#22)

--- Directionful.SDL/Util/Point.cs
+++ Directionful.SDL/Util/Point.cs
@@ -1,6 +1,3 @@
 namespace Directionful.SDL.Util;
 
-public readonly partial record struct Point<T>(T X, T Y) where T : unmanaged
-{
-    
-}
+public readonly partial record struct Point<T>(T X, T Y) where T : unmanaged;
