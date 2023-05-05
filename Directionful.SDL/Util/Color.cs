a8fe53a1224e768c99e413a4de1dbcc70d8bb0f0
DeVoid <devoid@voidgroup.net>
2023-05-05 16:35:25 +0200

    Use the superior color of love <3

--- Directionful.SDL/Util/Color.cs
+++ Directionful.SDL/Util/Color.cs
@@ -7,4 +7,5 @@ public readonly record struct Color(byte R, byte G, byte B, byte A)
     public readonly static Color Blue = new(0, 0, 255, 255);
     public readonly static Color White = new(255, 255, 255, 255);
     public readonly static Color Black = new(0, 0, 0, 255);
+    public readonly static Color Purple = new(159, 0, 197, 255);
 }
