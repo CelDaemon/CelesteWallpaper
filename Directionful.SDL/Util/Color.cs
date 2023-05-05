b8d36d4c6ee74c2b8be8ccedc9ce013b19a504c9
CelDaemon <celdaemon@voidgroup.net>
2023-05-05 15:04:02 +0200

    Add Basic SDL Rendering Support (#22)

---
+++ Directionful.SDL/Util/Color.cs
@@ -0,0 +1,10 @@
+namespace Directionful.SDL.Util;
+
+public readonly record struct Color(byte R, byte G, byte B, byte A)
+{
+    public readonly static Color Red = new(255, 0, 0, 255);
+    public readonly static Color Green = new(0, 255, 0, 255);
+    public readonly static Color Blue = new(0, 0, 255, 255);
+    public readonly static Color White = new(255, 255, 255, 255);
+    public readonly static Color Black = new(0, 0, 0, 255);
+}
--- Directionful.SDL/Util/Rectangle.cs
+++ Directionful.SDL/Util/Rectangle.cs
@@ -1,3 +1,3 @@
 namespace Directionful.SDL.Util;
 
-public readonly record struct Rectangle<T>(T X, T Y, T Width, T Height);
+public readonly record struct Rectangle<T>(T X, T Y, T Width, T Height) where T : unmanaged;
