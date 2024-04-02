925d645cd362847ffc73bf16f837c3177616f294
Celeste <devoid@voidgroup.net>
2024-04-02 11:26:31 +0200

    Update Program.cs

--- Directionful/Program.cs
+++ Directionful/Program.cs
@@ -9,7 +9,7 @@ GCSettings.LatencyMode = GCLatencyMode.LowLatency;
 using var sdl = new Sdl();
 using var video = sdl.Video;
 // ReSharper disable once StringLiteralTypo
-using var window = new Window(video, "Directionful - I love you so muchhh Kay <3", new Rectangle<int>(320, 180, 1280, 720));
+using var window = new Window(video, "Directionful - Celeste", new Rectangle<int>(320, 180, 1280, 720));
 using var renderer = window.Renderer;
 using var evt = sdl.Event;
 var running = true;
